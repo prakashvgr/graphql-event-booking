049a810d41987ec64a619c4244f3517bdafbf023
R <prakash.rajendran@capgemini.com>
2020-05-13 18:47:10 +0530

    changes related to types and data

--- app.js
+++ app.js
@@ -4,17 +4,32 @@ const graphqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
 
 const app = express()
-
+const events = []
 app.use(bodyParser.json())
 
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
+            type Event {
+                _id: ID!
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+
+            input EventInput {
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+
             type RootQuery {
-                events: [String!]!
+                events: [Event!]!
             }
 
             type RootMutation {
-                createEvents(name: String!): String
+                createEvent(eventInput: EventInput): Event
             }
 
             schema {
@@ -24,11 +39,18 @@ app.use('/graphql', graphqlHttp({
         `),
     rootValue: {
         events: () => {
-            return ['Romatic Cooking', 'Sailing', 'All-Night Coding']
+            return events
         },
-        createEvents: (args) => {
-            const eventName = args.name
-            return eventName
+        createEvent: args => {
+            const event = {
+                _id: Math.random.toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date().toISOString()
+            }
+            events.push(event)
+            return event
         }
     },
     graphiql: true
