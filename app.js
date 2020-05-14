95ec1017884ec318f87da9f8f1542bb3010c80a4
R <prakash.rajendran@capgemini.com>
2020-05-14 12:21:31 +0530

    Updated connect between mongoDB with graphQL

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const bodyParser =  require('body-parser')
+const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
+const mongoose = require('mongoose')
+const Event = require('./models/event')
 
 const app = express()
-const events = []
+
 app.use(bodyParser.json())
 
 app.use('/graphql', graphqlHttp({
@@ -39,22 +41,44 @@ app.use('/graphql', graphqlHttp({
         `),
     rootValue: {
         events: () => {
-            return events
+            return Event.find()
+                .then(events => {
+                    return events.map(event => {
+                        return { ...event._doc}
+                    })
+                })
+                .catch(err => {
+                    return err
+                })
         },
         createEvent: args => {
-            const event = {
-                _id: Math.random.toString(),
+            const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: new Date().toISOString()
-            }
-            events.push(event)
+                date: new Date(args.eventInput.date)
+            })
             return event
+                .save()
+                .then(result => {
+                    return {...result._doc}
+                }).catch(err => {
+                    throw err
+                })
         }
     },
     graphiql: true
 })
 )
 
-app.listen(3000)
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+        process.env.MONGO_PASSWORD
+    }@cluster0-gqu2f.gcp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+)
+.then(() => {
+    app.listen(3000)
+})
+.catch(err => {
+    throw err
+})
