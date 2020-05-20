84ad80fee42bc938bece3895e6d710e7a39f6871
R <prakash.rajendran@capgemini.com>
2020-05-20 10:17:46 +0530

    Updated with frontend logic

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -1,21 +1,43 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-import './MainNavigation.css'
+import AuthContext from '../../context/auth-context';
+import './MainNavigation.css';
 
-const MainNavigation = props => (
-    <header className="main-navigation">
-        <div className="main-navigation_logo">
+const mainNavigation = props => (
+  <AuthContext.Consumer>
+    {context => {
+      return (
+        <header className="main-navigation">
+          <div className="main-navigation__logo">
             <h1>EasyEvent</h1>
-        </div>
-        <nav className="main-navigation_items">
+          </div>
+          <nav className="main-navigation__items">
             <ul>
-                <li><NavLink to="/auth">Authenticate</NavLink></li>
-                <li><NavLink to="/events">Events</NavLink></li>
-                <li><NavLink to="/bookings">Bookings</NavLink></li>
+              {!context.token && (
+                <li>
+                  <NavLink to="/auth">Authenticate</NavLink>
+                </li>
+              )}
+              <li>
+                <NavLink to="/events">Events</NavLink>
+              </li>
+              {context.token && (
+                <React.Fragment>
+                  <li>
+                    <NavLink to="/bookings">Bookings</NavLink>
+                  </li>
+                  <li>
+                    <button onClick={context.logout}>Logout</button>
+                  </li>
+                </React.Fragment>
+              )}
             </ul>
-        </nav>
-    </header>
-)
+          </nav>
+        </header>
+      );
+    }}
+  </AuthContext.Consumer>
+);
 
-export default MainNavigation
+export default mainNavigation;
