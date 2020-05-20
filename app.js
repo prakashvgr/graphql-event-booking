91dde2bdec1611fe34d1b1f323d4078a4e5989be
R <prakash.rajendran@capgemini.com>
2020-05-20 09:43:12 +0530

    API integration with UI interface

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ const app = express()
 
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+})
+
 app.use(isAuth)
 
 app.use('/graphql', graphqlHttp({
@@ -22,12 +32,12 @@ app.use('/graphql', graphqlHttp({
 
 mongoose.connect(
     `mongodb+srv://${process.env.MONGO_USER}:${
-        process.env.MONGO_PASSWORD
+    process.env.MONGO_PASSWORD
     }@cluster0-gqu2f.gcp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 )
-.then(() => {
-    app.listen(3000)
-})
-.catch(err => {
-    throw err
-})
+    .then(() => {
+        app.listen(8000)
+    })
+    .catch(err => {
+        throw err
+    })
---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
+
+import AuthPage from './pages/Auth'
+import EventsPage from './pages/Events'
+import BookingsPage from './pages/Bookings'
+import MainNavigation from './components/Navigation/MainNavigation'
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact ></Redirect>
+              <Route path="/auth" component={AuthPage}></Route>
+              <Route path="/events" component={EventsPage}></Route>
+              <Route path="/bookings" component={BookingsPage}></Route>
+            </Switch>
+          </main>
+        </React.Fragment>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default App;
